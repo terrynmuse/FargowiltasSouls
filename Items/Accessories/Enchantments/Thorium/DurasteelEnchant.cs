6c7ef3cb21a0150912bf3e3abeca461ee9d1d313
Liam <leeleegill20@gmail.com>
2018-10-08 11:49:10 -0400

    Create DurasteelEnchant.cs

---
+++ Items/Accessories/Enchantments/Thorium/DurasteelEnchant.cs
@@ -0,0 +1,68 @@
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace FargowiltasSouls.Items.Accessories.Enchantments.Thorium
+{
+    public class DurasteelEnchant : ModItem
+    {
+        private readonly Mod thorium = ModLoader.GetMod("ThoriumMod");
+        
+        public override bool Autoload(ref string name)
+        {
+            return ModLoader.GetLoadedMods().Contains("ThoriumMod");
+        }
+        
+        public override string Texture => "FargowiltasSouls/Items/Placeholder";
+        
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Durasteel Enchantment");
+            Tooltip.SetDefault(
+                @"'Masterfully forged by the Blacksmith'
+");
+        }
+
+        public override void SetDefaults()
+        {
+            item.width = 20;
+            item.height = 20;
+            item.accessory = true;
+            ItemID.Sets.ItemNoGravity[item.type] = true;
+            item.rare = 3;
+            item.value = 80000;
+        }
+
+        public override void UpdateAccessory(Player player, bool hideVisual)
+        {
+            if (!Fargowiltas.Instance.ThoriumLoaded) return;
+            
+            DurasteelEffect(player);
+        }
+        
+        private void DurasteelEffect(Player player)
+        {
+            ThoriumPlayer thoriumPlayer = player.GetModPlayer<ThoriumPlayer>(thorium);
+            
+            
+        }
+        
+        private readonly string[] items =
+        {
+            ""
+        };
+
+        public override void AddRecipes()
+        {
+            if (!Fargowiltas.Instance.ThoriumLoaded) return;
+            
+            ModRecipe recipe = new ModRecipe(mod);
+            
+            foreach (string i in items) recipe.AddIngredient(thorium.ItemType(i));
+
+            recipe.AddTile(TileID.DemonAltar);
+            recipe.SetResult(this);
+            recipe.AddRecipe();
+        }
+    }
+}
