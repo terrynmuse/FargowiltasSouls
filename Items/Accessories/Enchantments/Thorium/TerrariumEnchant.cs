8778ac747695866c92bec4506f008ccab67270d1
Liam <leeleegill20@gmail.com>
2018-10-08 13:23:22 -0400

    Create TerrariumEnchant.cs

---
+++ Items/Accessories/Enchantments/Thorium/TerrariumEnchant.cs
@@ -0,0 +1,68 @@
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace FargowiltasSouls.Items.Accessories.Enchantments.Thorium
+{
+    public class TerrariumEnchant : ModItem
+    {
+        private readonly Mod thorium = ModLoader.GetMod("ThoriumMod");
+        
+        public override bool Autoload(ref string name)
+        {
+            return ModLoader.GetLoadedMods().Contains("ThoriumMod");
+        }
+        
+        public override string Texture => "FargowiltasSouls/Items/Placeholder";
+        
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Terrarium Enchantment");
+            Tooltip.SetDefault(
+                @"'All will fall before your might...'
+");
+        }
+
+        public override void SetDefaults()
+        {
+            item.width = 20;
+            item.height = 20;
+            item.accessory = true;
+            ItemID.Sets.ItemNoGravity[item.type] = true;
+            item.rare = 2; //rainbow
+            item.value = 250000;
+        }
+
+        public override void UpdateAccessory(Player player, bool hideVisual)
+        {
+            if (!Fargowiltas.Instance.ThoriumLoaded) return;
+            
+            TerrariumEffect(player);
+        }
+        
+        private void TerrariumEffect(Player player)
+        {
+            ThoriumPlayer thoriumPlayer = player.GetModPlayer<ThoriumPlayer>(thorium);
+            
+            
+        }
+        
+        private readonly string[] items =
+        {
+            ""
+        };
+
+        public override void AddRecipes()
+        {
+            if (!Fargowiltas.Instance.ThoriumLoaded) return;
+            
+            ModRecipe recipe = new ModRecipe(mod);
+            
+            foreach (string i in items) recipe.AddIngredient(thorium.ItemType(i));
+
+            recipe.AddTile(TileID.DemonAltar);
+            recipe.SetResult(this);
+            recipe.AddRecipe();
+        }
+    }
+}
