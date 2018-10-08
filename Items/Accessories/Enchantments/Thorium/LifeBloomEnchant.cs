f6adbff6026d3e676961fd2651fdfb0699e76088
Liam <leeleegill20@gmail.com>
2018-10-08 13:03:29 -0400

    Create LifeBloomEnchant.cs

---
+++ Items/Accessories/Enchantments/Thorium/LifeBloomEnchant.cs
@@ -0,0 +1,68 @@
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace FargowiltasSouls.Items.Accessories.Enchantments.Thorium
+{
+    public class LifeBloomEnchant : ModItem
+    {
+        private readonly Mod thorium = ModLoader.GetMod("ThoriumMod");
+        
+        public override bool Autoload(ref string name)
+        {
+            return ModLoader.GetLoadedMods().Contains("ThoriumMod");
+        }
+        
+        public override string Texture => "FargowiltasSouls/Items/Placeholder";
+        
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Life Bloom Enchantment");
+            Tooltip.SetDefault(
+                @"'You are one with nature'
+");
+        }
+
+        public override void SetDefaults()
+        {
+            item.width = 20;
+            item.height = 20;
+            item.accessory = true;
+            ItemID.Sets.ItemNoGravity[item.type] = true;
+            item.rare = 7;
+            item.value = 200000;
+        }
+
+        public override void UpdateAccessory(Player player, bool hideVisual)
+        {
+            if (!Fargowiltas.Instance.ThoriumLoaded) return;
+            
+            LifeBloomEffect(player);
+        }
+        
+        private void LifeBloomEffect(Player player)
+        {
+            ThoriumPlayer thoriumPlayer = player.GetModPlayer<ThoriumPlayer>(thorium);
+            
+            
+        }
+        
+        private readonly string[] items =
+        {
+            ""
+        };
+
+        public override void AddRecipes()
+        {
+            if (!Fargowiltas.Instance.ThoriumLoaded) return;
+            
+            ModRecipe recipe = new ModRecipe(mod);
+            
+            foreach (string i in items) recipe.AddIngredient(thorium.ItemType(i));
+
+            recipe.AddTile(TileID.DemonAltar);
+            recipe.SetResult(this);
+            recipe.AddRecipe();
+        }
+    }
+}
