7df9d0f6dbe938724d46b85af983ae8876e26802
Liam <leeleegill20@gmail.com>
2018-10-09 10:52:04 -0400

    Create BiotechEnchant.cs

---
+++ Items/Accessories/Enchantments/Thorium/BiotechEnchant.cs
@@ -0,0 +1,68 @@
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace FargowiltasSouls.Items.Accessories.Enchantments.Thorium
+{
+    public class BiotechEnchant : ModItem
+    {
+        private readonly Mod thorium = ModLoader.GetMod("ThoriumMod");
+        
+        public override bool Autoload(ref string name)
+        {
+            return ModLoader.GetLoadedMods().Contains("ThoriumMod");
+        }
+        
+        public override string Texture => "FargowiltasSouls/Items/Placeholder";
+        
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Biotech Enchantment");
+            Tooltip.SetDefault(
+                @"''
+");
+        }
+
+        public override void SetDefaults()
+        {
+            item.width = 20;
+            item.height = 20;
+            item.accessory = true;
+            ItemID.Sets.ItemNoGravity[item.type] = true;
+            item.rare = 6;
+            item.value = 150000;
+        }
+
+        public override void UpdateAccessory(Player player, bool hideVisual)
+        {
+            if (!Fargowiltas.Instance.ThoriumLoaded) return;
+            
+            BiotechEffect(player);
+        }
+        
+        private void BiotechEffect(Player player)
+        {
+            ThoriumPlayer thoriumPlayer = player.GetModPlayer<ThoriumPlayer>(thorium);
+            
+            
+        }
+        
+        private readonly string[] items =
+        {
+            ""
+        };
+
+        public override void AddRecipes()
+        {
+            if (!Fargowiltas.Instance.ThoriumLoaded) return;
+            
+            ModRecipe recipe = new ModRecipe(mod);
+            
+            foreach (string i in items) recipe.AddIngredient(thorium.ItemType(i));
+
+            recipe.AddTile(TileID.DemonAltar);
+            recipe.SetResult(this);
+            recipe.AddRecipe();
+        }
+    }
+}
