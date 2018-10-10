80d86493c2f38b5fcafc201895cba4bd64231eea
Liam <leeleegill20@gmail.com>
2018-10-10 10:17:26 -0400

    Create BardEssence.cs

---
+++ Items/Accessories/Essences/BardEssence.cs
@@ -0,0 +1,83 @@
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using System.Linq;
+using ThoriumMod;
+
+namespace FargowiltasSouls.Items.Accessories.Essences
+{
+    public class BardEssence : ModItem
+    {
+        private readonly Mod thorium = ModLoader.GetMod("ThoriumMod");
+        
+        public override bool Autoload(ref string name)
+        {
+            return ModLoader.GetLoadedMods().Contains("ThoriumMod");
+        }
+        
+        public override string Texture => "FargowiltasSouls/Items/Placeholder";
+        
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Bard Essence");
+            Tooltip.SetDefault(
+                @"''
+");
+        }
+
+        public override void SetDefaults()
+        {
+            item.width = 20;
+            item.height = 20;
+            item.accessory = true;
+            ItemID.Sets.ItemNoGravity[item.type] = true; //
+            item.rare = 2; //
+            item.value = 60000; //
+        }
+
+        public override void UpdateAccessory(Player player, bool hideVisual)
+        {
+            if (!Fargowiltas.Instance.ThoriumLoaded) return;
+            
+            BardEffect(player);
+        }
+        
+        private void BardEffect(Player player)
+        {
+            ThoriumPlayer thoriumPlayer = player.GetModPlayer<ThoriumPlayer>(thorium);
+            
+            
+        }
+        
+        private readonly string[] items =
+        {
+            BardEmblem
+            SongofIceandFire
+            BagPipe
+            SeashellCastanets
+            Didgeridoo
+            YewWoodLute
+            AntlionMaraca
+            MusicSheetCongas
+            MusicSheetSteelDrum
+            BronzeTuningFork
+            GraniteBoombox
+            ForestOcarina
+            HotHorn
+            SonarCannon
+        };
+
+        public override void AddRecipes()
+        {
+            if (!Fargowiltas.Instance.ThoriumLoaded) return;
+            
+            ModRecipe recipe = new ModRecipe(mod);
+            
+            foreach (string i in items) recipe.AddIngredient(thorium.ItemType(i));
+
+            recipe.AddTile(TileID.DemonAltar);
+            recipe.SetResult(this);
+            recipe.AddRecipe();
+        }
+    }
+}
