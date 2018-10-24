16d85b5c34adc875b7b9224dd8cee4597854cb97
Liam <leeleegill20@gmail.com>
2018-10-24 12:32:36 -0400

    Update ObsidianEnchant.cs

--- Items/Accessories/Enchantments/ObsidianEnchant.cs
+++ Items/Accessories/Enchantments/ObsidianEnchant.cs
@@ -37,17 +37,23 @@ While standing in lava, you gain 10 more armor penetration, 10% attack speed, an
             recipe.AddIngredient(ItemID.ObsidianHelm);
             recipe.AddIngredient(ItemID.ObsidianShirt);
             recipe.AddIngredient(ItemID.ObsidianPants);
-            recipe.AddIngredient(ItemID.ObsidianRose);
-            recipe.AddIngredient(ItemID.LavaWaders);
-            recipe.AddIngredient(ItemID.SharkToothNecklace); //hmmm reee
             
-            /*
-ObsidianScale
-ObsidianButterfly
-Obsidian armor thorium
-Obsidian armor thorium
-Obsidian armor thorium
-            */
+            if(Fargowiltas.Instance.ThoriumLoaded)
+            {      
+                recipe.AddIngredient(thorium.ItemType("AncientObsidianHelmet"));
+                recipe.AddIngredient(thorium.ItemType("AncientObsidianChestguard"));
+                recipe.AddIngredient(thorium.ItemType("AncientObsidianGreaves"));
+                recipe.AddIngredient(thorium.ItemType("ObsidianScale"));
+                recipe.AddIngredient(ItemID.ObsidianRose);
+                recipe.AddIngredient(ItemID.SharkToothNecklace);
+                recipe.AddIngredient(thorium.ItemType("ObsidianButterfly"));
+            }
+            else
+            {
+                recipe.AddIngredient(ItemID.ObsidianRose);
+                recipe.AddIngredient(ItemID.LavaWaders);
+                recipe.AddIngredient(ItemID.SharkToothNecklace);
+            }
             
             recipe.AddTile(TileID.DemonAltar);
             recipe.SetResult(this);
