e91f6cdcaf3ca072211dacbbda6efc851c609276
Liam <leeleegill20@gmail.com>
2018-10-24 14:10:04 -0400

    Update SolarEnchant.cs

--- Items/Accessories/Enchantments/SolarEnchant.cs
+++ Items/Accessories/Enchantments/SolarEnchant.cs
@@ -37,18 +37,24 @@ Melee attacks inflict it for less time (which is a good thing)");
             recipe.AddIngredient(ItemID.SolarFlareHelmet);
             recipe.AddIngredient(ItemID.SolarFlareBreastplate);
             recipe.AddIngredient(ItemID.SolarFlareLeggings);
-            recipe.AddIngredient(ItemID.HelFire);
+            
+            if(Fargowiltas.Instance.ThoriumLoaded)
+            {      
+                recipe.AddIngredient(ItemID.SolarWings);
+                recipe.AddIngredient(ItemID.HelFire);
+                recipe.AddIngredient(thorium.ItemType("BlackBlade"));
+                recipe.AddIngredient(thorium.ItemType("EruptingFlare"));
+            }
+            else
+            {
+                recipe.AddIngredient(ItemID.HelFire);
+            }
+            
             recipe.AddIngredient(ItemID.SolarEruption);
             recipe.AddIngredient(ItemID.DayBreak);
             recipe.AddIngredient(ItemID.StarWrath);
+
             recipe.AddTile(TileID.LunarCraftingStation);
-
-            /*
-SolarWings
-BlackBlade
-EruptingFlare
-             */
-
             recipe.SetResult(this);
             recipe.AddRecipe();
         }
