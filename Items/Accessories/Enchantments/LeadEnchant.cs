af3aa41f6581343326448cdf3ea370aa6f39becf
Liam <leeleegill20@gmail.com>
2018-10-24 11:30:05 -0400

    Update LeadEnchant.cs

--- Items/Accessories/Enchantments/LeadEnchant.cs
+++ Items/Accessories/Enchantments/LeadEnchant.cs
@@ -36,17 +36,24 @@ Lead Poisoning deals damage over time and slows enemies slightly");
             recipe.AddIngredient(ItemID.LeadHelmet);
             recipe.AddIngredient(ItemID.LeadChainmail);
             recipe.AddIngredient(ItemID.LeadGreaves);
-            recipe.AddIngredient(ItemID.LeadShortsword);
-            recipe.AddIngredient(ItemID.LeadPickaxe);
-            recipe.AddIngredient(ItemID.GrayPaint);
-
-            /*
-LeadShield
-OnyxStaff
-OnyxButterfly
-PoisonKnives
-             */
-
+            
+            if(Fargowiltas.Instance.ThoriumLoaded)
+            {      
+                recipe.AddIngredient(thorium.ItemType("LeadShield"));
+                recipe.AddIngredient(ItemID.LeadShortsword);
+                recipe.AddIngredient(ItemID.LeadPickaxe);
+                recipe.AddIngredient(thorium.ItemType("OnyxStaff"));
+                recipe.AddIngredient(thorium.ItemType("RustySword"));
+                recipe.AddIngredient(ItemID.GrayPaint);
+                recipe.AddIngredient(thorium.ItemType("OnyxButterfly"));
+            }
+            else
+            {
+                recipe.AddIngredient(ItemID.LeadShortsword);
+                recipe.AddIngredient(ItemID.LeadPickaxe);
+                recipe.AddIngredient(ItemID.GrayPaint);
+            }
+            
             recipe.AddTile(TileID.DemonAltar);
             recipe.SetResult(this);
             recipe.AddRecipe();
