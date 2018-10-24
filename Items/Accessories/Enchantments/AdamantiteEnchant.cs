9d4ee08dda1b5fa30939ef8b6b1806a62b495136
Liam <leeleegill20@gmail.com>
2018-10-24 09:07:31 -0400

    Update AdamantiteEnchant.cs

--- Items/Accessories/Enchantments/AdamantiteEnchant.cs
+++ Items/Accessories/Enchantments/AdamantiteEnchant.cs
@@ -10,7 +10,7 @@ namespace FargowiltasSouls.Items.Accessories.Enchantments
         {
             DisplayName.SetDefault("Adamantite Enchantment");
             Tooltip.SetDefault(
-                @"'Who needs to aim?' 
+                @"'Who needs to aim?'
 Every 8th projectile you shoot will split into 3
 Any secondary projectiles may also split");
         }
@@ -36,18 +36,23 @@ Any secondary projectiles may also split");
             recipe.AddRecipeGroup("FargowiltasSouls:AnyAdamHead");
             recipe.AddIngredient(ItemID.AdamantiteBreastplate);
             recipe.AddIngredient(ItemID.AdamantiteLeggings);
-            recipe.AddIngredient(ItemID.AdamantiteGlaive);
-            recipe.AddIngredient(ItemID.DarkLance);
+            
+            if(Fargowiltas.Instance.ThoriumLoaded)
+            {      
+                recipe.AddIngredient(ItemID.AdamantiteGlaive);
+                recipe.AddIngredient(thorium.ItemType("AdamantiteStaff"));
+                recipe.AddIngredient(thorium.ItemType("DynastyWarFan"));
+                recipe.AddIngredient(thorium.ItemType("Scorn"));
+                recipe.AddIngredient(thorium.ItemType("OgreSnotGun"));
+            }
+            else
+            {
+                recipe.AddIngredient(ItemID.DarkLance);
+                recipe.AddIngredient(ItemID.AdamantiteGlaive);
+            }
+            
             recipe.AddIngredient(ItemID.Shotgun);
             recipe.AddIngredient(ItemID.VenomStaff);
-            
-            /*
-OgreSnotGun
-AdamantiteStaff
-AdamantiteCarbine
-DynastyWarFan
-Scorn
-            */
             
             recipe.AddTile(TileID.CrystalBall);
             recipe.SetResult(this);
