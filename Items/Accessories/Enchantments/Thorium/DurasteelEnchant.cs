f925de54d164f7de7f52e5f8fc6c52196d67b7e1
Liam <leeleegill20@gmail.com>
2018-10-30 14:38:17 -0400

    Update DurasteelEnchant.cs

--- Items/Accessories/Enchantments/Thorium/DurasteelEnchant.cs
+++ Items/Accessories/Enchantments/Thorium/DurasteelEnchant.cs
@@ -22,7 +22,8 @@ namespace FargowiltasSouls.Items.Accessories.Enchantments.Thorium
             DisplayName.SetDefault("Durasteel Enchantment");
             Tooltip.SetDefault(
                 @"'Masterfully forged by the Blacksmith'
-+12% damage reduction");
++12% damage reduction
+Magnetizes all loose items on the screen");
         }
 
         public override void SetDefaults()
