e0d30368243bacfd17fa27ddf62a6af91d7270ad
unknown <leeleegill20@gmail.com>
2019-01-04 14:47:42 -0500

    New VS project, get REKT 56

---
+++ FargoGlobalTile.cs
@@ -0,0 +1,17 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace Fargowiltas.Tiles
+{
+    public class FargoGlobalTile : GlobalTile
+    {
+        /*public override void NearbyEffects(int i, int j, int type, bool closer)
+        {
+            if(type == TileID.BeeHive)
+            {
+                NPC.NewNPC(Main.tile[i, j].frameX, Main.tile[i, j].frameY, NPCID.BeeSmall);
+            }
+        }*/
+    }
+}
