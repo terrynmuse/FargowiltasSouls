8aca29733345f6ba05e573c766e96f4a08e849c6
Liam <leeleegill20@gmail.com>
2019-04-18 15:56:48 -0400

    Update SilverEnchant.cs

--- Items/Accessories/Enchantments/SilverEnchant.cs
+++ Items/Accessories/Enchantments/SilverEnchant.cs
@@ -89,6 +89,7 @@ Summons a sword familiar that scales with minion damage";
             {
                 recipe.AddIngredient(ItemID.SilverBroadsword);
                 recipe.AddIngredient(ItemID.SapphireStaff);
+                recipe.AddIngredient(ItemID.BluePhaseblade);
                 recipe.AddIngredient(ItemID.AquaScepter);
             }
 
