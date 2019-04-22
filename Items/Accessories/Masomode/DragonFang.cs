265e9fb188d253d89b1589d90041d72ddb028dd4
Liam <leeleegill20@gmail.com>
2019-04-22 11:52:27 -0400

    Create DragonFang.cs

---
+++ Items/Accessories/Masomode/DragonFang.cs
@@ -0,0 +1,40 @@
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace FargowiltasSouls.Items.Accessories.Masomode
+{
+    public class SqueakyToy : ModItem
+    {
+        public override string Texture => "FargowiltasSouls/Items/Placeholder";
+        
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Dragon's Fang");
+            Tooltip.SetDefault(@"''
+Grants immunity to Clipped Wings and Crippled
+something dumb, a wyvern summon? ohno or necro enchant but its a wyvern or just shoot dragon fangs while attacking idk ech
+");
+        }
+
+        public override void SetDefaults()
+        {
+            item.width = 20;
+            item.height = 20;
+            item.accessory = true;
+            item.rare = 4;
+            item.value = Item.sellPrice(0, 8);
+        }
+
+        public override void UpdateAccessory(Player player, bool hideVisual)
+        {
+            player.buffImmune[mod.BuffType("ClippedWings")] = true;
+            player.buffImmune[mod.BuffType("Crippled")] = true;
+            
+            //
+
+            
+        }
+    }
+}
