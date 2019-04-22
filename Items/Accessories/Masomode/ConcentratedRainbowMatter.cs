06c2e8a93cbc9cd8ffbb23c9befaacb7ca4d9e1f
Liam <leeleegill20@gmail.com>
2019-04-22 11:55:59 -0400

    Create ConcentratedRainbowMatter.cs

---
+++ Items/Accessories/Masomode/ConcentratedRainbowMatter.cs
@@ -0,0 +1,40 @@
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace FargowiltasSouls.Items.Accessories.Masomode
+{
+    public class ConcentratedRainbowMatter : ModItem
+    {
+        public override string Texture => "FargowiltasSouls/Items/Placeholder";
+        
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Concentrated Rainbow Matter");
+            Tooltip.SetDefault(@"''
+Grants immunity to Flames of the Universe
+Summons a rainbow slime (which is just slime AI but inflicts Flames of the Universe and is bigger maybe, 
+additionally if he has a target but hasnt hit them in so long, he jumps super high to them or shoots some rainbow shet)
+");
+        }
+
+        public override void SetDefaults()
+        {
+            item.width = 20;
+            item.height = 20;
+            item.accessory = true;
+            item.rare = 4;
+            item.value = Item.sellPrice(0, 8);
+        }
+
+        public override void UpdateAccessory(Player player, bool hideVisual)
+        {
+            //player.buffImmune[mod.BuffType("FlamesoftheUniverse")] = true;
+            
+            //
+
+            
+        }
+    }
+}
