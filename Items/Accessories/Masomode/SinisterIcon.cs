c479dc0b6f3e6977cb14c30de836a9716740106e
Terry N. Muse <darius.loyhrs@hotmail.com>
2019-09-09 19:02:23 -0400

    placeholder sprite for computation orb sprites for necro brew/sinister icon

--- Items/Accessories/Masomode/SinisterIcon.cs
+++ Items/Accessories/Masomode/SinisterIcon.cs
@@ -6,8 +6,6 @@ namespace FargowiltasSouls.Items.Accessories.Masomode
 {
     public class SinisterIcon : ModItem
     {
-        public override string Texture => "FargowiltasSouls/Items/Placeholder";
-        
         public override void SetStaticDefaults()
         {
             DisplayName.SetDefault("Sinister Icon");
