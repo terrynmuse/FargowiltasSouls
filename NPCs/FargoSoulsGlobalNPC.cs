064cabde890b50898070acc3bef08d4f04f5e52f
Terry N. Muse <darius.loyhrs@hotmail.com>
2019-09-25 18:04:31 -0400

    dsiable resprite

--- NPCs/FargoSoulsGlobalNPC.cs
+++ NPCs/FargoSoulsGlobalNPC.cs
@@ -10775,7 +10775,7 @@ namespace FargowiltasSouls.NPCs
 
         private void LoadSprites(NPC npc)
         {
-            bool recolor = SoulConfig.Instance.masoTogDict.ContainsKey("Boss Recolors (Restart to use)") && !SoulConfig.Instance.masoTogDict["Boss Recolors (Restart to use)"] && FargoSoulsWorld.MasochistMode;
+            /*bool recolor = SoulConfig.Instance.masoTogDict.ContainsKey("Boss Recolors (Restart to use)") && !SoulConfig.Instance.masoTogDict["Boss Recolors (Restart to use)"] && FargoSoulsWorld.MasochistMode;
             if (recolor || Fargowiltas.Instance.LoadedNewSprites)
             {
                 Fargowiltas.Instance.LoadedNewSprites = true;
@@ -10929,7 +10929,7 @@ namespace FargowiltasSouls.NPCs
                     default:
                         break;
                 }
-            }
+            }*/
         }
     }
 }
