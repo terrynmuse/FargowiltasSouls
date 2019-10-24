d8cc0350c7715d9bac389326bae066347211ffbe
terrynmuse <darius.loyhrs@hotmail.com>
2019-10-24 10:22:15 -0400

    added optic staff?

---
+++ Projectiles/Minions/OpticLaser.cs
@@ -0,0 +1,44 @@
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace FargowiltasSouls.Projectiles.Minions
+{
+    public class OpticLaser : ModProjectile
+    {
+        public override string Texture => "Terraria/Projectile_100"; 
+
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Death Laser");
+        }
+
+        public override void SetDefaults()
+        {
+            projectile.CloneDefaults(ProjectileID.DeathLaser);
+            aiType = ProjectileID.DeathLaser;
+            projectile.hostile = false;
+            projectile.friendly = true;
+            projectile.minion = true;
+            projectile.magic = false;
+            projectile.tileCollide = false;
+            projectile.penetrate = -1;
+            projectile.ignoreWater = true;
+
+            projectile.usesIDStaticNPCImmunity = true;
+            projectile.idStaticNPCHitCooldown = 6;
+        }
+
+        public override void OnHitNPC(NPC target, int damage, float knockback, bool crit)
+        {
+            target.AddBuff(BuffID.Ichor, 600);
+        }
+
+        public override Color? GetAlpha(Color lightColor)
+        {
+            return Color.White;
+        }
+    }
+}
---
+++ Projectiles/Minions/OpticSpazmatism.cs
@@ -0,0 +1,193 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace FargowiltasSouls.Projectiles.Minions
+{
+    public class OpticSpazmatism : ModProjectile
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Spazmatism");
+            Main.projFrames[projectile.type] = 6;
+            ProjectileID.Sets.TrailCacheLength[projectile.type] = 6;
+            ProjectileID.Sets.TrailingMode[projectile.type] = 2;
+        }
+
+        public override void SetDefaults()
+        {
+            projectile.width = 100;
+            projectile.height = 110;
+            projectile.ignoreWater = true;
+            projectile.tileCollide = false;
+            projectile.friendly = true;
+            projectile.minion = true;
+            projectile.minionSlots = 2;
+            projectile.penetrate = -1;
+            projectile.tileCollide = false;
+            projectile.ignoreWater = true;
+            projectile.aiStyle = -1;
+            projectile.netImportant = true;
+            projectile.scale = .75f;
+
+            projectile.usesLocalNPCImmunity = true;
+            projectile.localNPCHitCooldown = 6;
+        }
+
+        public override void AI()
+        {
+            Player player = Main.player[projectile.owner];
+            if (player.active && !player.dead && player.GetModPlayer<FargoPlayer>().TwinsEX)
+                projectile.timeLeft = 2;
+
+            if (projectile.ai[0] >= 0 && projectile.ai[0] < 200) //has target
+            {
+                NPC minionAttackTargetNpc = projectile.OwnerMinionAttackTargetNPC;
+                if (minionAttackTargetNpc != null && projectile.ai[0] != minionAttackTargetNpc.whoAmI && minionAttackTargetNpc.CanBeChasedBy(projectile))
+                    projectile.ai[0] = minionAttackTargetNpc.whoAmI;
+                
+                NPC npc = Main.npc[(int)projectile.ai[0]];
+                if (npc.CanBeChasedBy(projectile))
+                {
+                    Vector2 targetPos = npc.Center + projectile.DirectionFrom(npc.Center) * 300;
+                    if (projectile.Distance(targetPos) > 50)
+                        Movement(targetPos, 0.7f);
+                    projectile.rotation = projectile.DirectionTo(npc.Center).ToRotation() - (float)Math.PI / 2;
+
+                    if (++projectile.localAI[0] > 20)
+                    {
+                        projectile.localAI[0] = 0;
+                        if (projectile.owner == Main.myPlayer)
+                            Projectile.NewProjectile(projectile.Center, projectile.DirectionTo(npc.Center) * 8, mod.ProjectileType("OpticLaser"), projectile.damage, projectile.knockBack, projectile.owner);
+                    }
+                }
+                else //forget target
+                {
+                    projectile.ai[0] = HomeOnTarget();
+                    projectile.netUpdate = true;
+                }
+            }
+            else //no target
+            {
+                Vector2 targetPos = player.Center;
+                targetPos.Y -= 100;
+
+                if (projectile.Distance(targetPos) > 3000)
+                    projectile.Center = player.Center;
+                else if (projectile.Distance(targetPos) > 200)
+                    Movement(targetPos, 0.7f);
+
+                projectile.rotation = projectile.velocity.ToRotation() - (float)Math.PI / 2;
+
+                if (++projectile.localAI[1] > 6)
+                {
+                    projectile.localAI[1] = 0;
+                    projectile.ai[0] = HomeOnTarget();
+                    if (projectile.ai[0] != -1)
+                        projectile.netUpdate = true;
+                }
+            }
+
+            if (++projectile.frameCounter > 4)
+            {
+                projectile.frameCounter = 0;
+                if (++projectile.frame >= 6)
+                    projectile.frame = 3;
+            }
+            if (projectile.frame < 3)
+                projectile.frame = 3;
+        }
+
+        private void Movement(Vector2 targetPos, float speedModifier)
+        {
+            if (projectile.Center.X < targetPos.X)
+            {
+                projectile.velocity.X += speedModifier;
+                if (projectile.velocity.X < 0)
+                    projectile.velocity.X += speedModifier * 2;
+            }
+            else
+            {
+                projectile.velocity.X -= speedModifier;
+                if (projectile.velocity.X > 0)
+                    projectile.velocity.X -= speedModifier * 2;
+            }
+            if (projectile.Center.Y < targetPos.Y)
+            {
+                projectile.velocity.Y += speedModifier;
+                if (projectile.velocity.Y < 0)
+                    projectile.velocity.Y += speedModifier * 2;
+            }
+            else
+            {
+                projectile.velocity.Y -= speedModifier;
+                if (projectile.velocity.Y > 0)
+                    projectile.velocity.Y -= speedModifier * 2;
+            }
+            if (Math.Abs(projectile.velocity.X) > 24)
+                projectile.velocity.X = 24 * Math.Sign(projectile.velocity.X);
+            if (Math.Abs(projectile.velocity.Y) > 24)
+                projectile.velocity.Y = 24 * Math.Sign(projectile.velocity.Y);
+        }
+
+        private int HomeOnTarget()
+        {
+            NPC minionAttackTargetNpc = projectile.OwnerMinionAttackTargetNPC;
+            if (minionAttackTargetNpc != null && minionAttackTargetNpc.CanBeChasedBy(projectile))
+                return minionAttackTargetNpc.whoAmI;
+
+            const float homingMaximumRangeInPixels = 2000;
+            int selectedTarget = -1;
+            for (int i = 0; i < Main.maxNPCs; i++)
+            {
+                NPC n = Main.npc[i];
+                if (n.CanBeChasedBy(projectile))
+                {
+                    float distance = projectile.Distance(n.Center);
+                    if (distance <= homingMaximumRangeInPixels &&
+                        (
+                            selectedTarget == -1 || //there is no selected target
+                            projectile.Distance(Main.npc[selectedTarget].Center) > distance) //or we are closer to this target than the already selected target
+                    )
+                        selectedTarget = i;
+                }
+            }
+
+            return selectedTarget;
+        }
+
+        public override void OnHitNPC(NPC target, int damage, float knockback, bool crit)
+        {
+            target.AddBuff(BuffID.Ichor, 600);
+        }
+
+        public override bool PreDraw(SpriteBatch spriteBatch, Color lightColor)
+        {
+            Texture2D texture2D13 = Main.projectileTexture[projectile.type];
+            int num156 = Main.projectileTexture[projectile.type].Height / Main.projFrames[projectile.type]; //ypos of lower right corner of sprite to draw
+            int y3 = num156 * projectile.frame; //ypos of upper left corner of sprite to draw
+            Rectangle rectangle = new Rectangle(0, y3, texture2D13.Width, num156);
+            Vector2 origin2 = rectangle.Size() / 2f;
+
+            Color color26 = lightColor;
+            color26 = projectile.GetAlpha(color26);
+
+            SpriteEffects effects = projectile.spriteDirection > 0 ? SpriteEffects.None : SpriteEffects.FlipHorizontally;
+
+            for (int i = 0; i < ProjectileID.Sets.TrailCacheLength[projectile.type]; i++)
+            {
+                Color color27 = color26 * 0.5f;
+                color27 *= (float)(ProjectileID.Sets.TrailCacheLength[projectile.type] - i) / ProjectileID.Sets.TrailCacheLength[projectile.type];
+                Vector2 value4 = projectile.oldPos[i];
+                float num165 = projectile.oldRot[i];
+                Main.spriteBatch.Draw(texture2D13, value4 + projectile.Size / 2f - Main.screenPosition + new Vector2(0, projectile.gfxOffY), new Microsoft.Xna.Framework.Rectangle?(rectangle), color27, num165, origin2, projectile.scale, effects, 0f);
+            }
+
+            Main.spriteBatch.Draw(texture2D13, projectile.Center - Main.screenPosition + new Vector2(0f, projectile.gfxOffY), new Microsoft.Xna.Framework.Rectangle?(rectangle), projectile.GetAlpha(lightColor), projectile.rotation, origin2, projectile.scale, effects, 0f);
+            return false;
+        }
+    }
+}
